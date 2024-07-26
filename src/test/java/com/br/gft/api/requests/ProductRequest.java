6442fb532f6bfa719cbd3ed359660128b0523290
erisson.batista@ask.com <erissonbreno@hotmail.com>
2024-07-25 22:58:51 -0300

    Get Product Api test

---
+++ src/test/java/com/br/gft/api/requests/ProductRequest.java
@@ -0,0 +1,19 @@
+package com.br.gft.api.requests;
+
+import com.br.gft.api.endpoints.Endpoints;
+import io.restassured.response.Response;
+
+import static com.br.gft.api.endpoints.Endpoints.BASE_URL;
+import static com.br.gft.api.endpoints.Endpoints.PRODUCTS_LIST;
+import static io.restassured.RestAssured.given;
+
+public class ProductRequest {
+
+    public static Response getProducts() {
+        return given()
+                .log().all()
+                .baseUri(BASE_URL)
+                .when()
+                .get(PRODUCTS_LIST);
+    }
+}
---
+++ src/test/java/com/br/gft/api/tests/ProductApiTest.java
@@ -0,0 +1,32 @@
+package com.br.gft.api.tests;
+
+import com.br.gft.api.requests.ProductRequest;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class ProductApiTest {
+
+    @Test
+    public void validateGetProducts() {
+        Response response = ProductRequest.getProducts();
+        JsonPath jsonResponse = response.jsonPath();
+
+        response.then()
+                .log().body()
+                .assertThat()
+                .statusCode(SC_OK);
+
+        assertThat(jsonResponse.getList("products").size(), greaterThan(0));
+        assertEquals("Blue Top", jsonResponse.get("products[0].name"));
+        assertEquals("Rs. 400", jsonResponse.get("products.find { it.name == 'Men Tshirt' }.price"));
+    }
+}
